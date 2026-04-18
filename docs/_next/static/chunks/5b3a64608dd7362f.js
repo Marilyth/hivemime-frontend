c5448e5be64a9015e60f8349e9db711e77418fdf
May <baermay98@gmail.com>
2026-04-18 10:09:05 +0200

    Rebuild

---
+++ docs/_next/static/chunks/5b3a64608dd7362f.js
@@ -0,0 +1,5 @@
+__turbopack_load_page_chunks__("/_app", [
+  "static/chunks/e60ef129113f6e24.js",
+  "static/chunks/b7a31621873cd83f.js",
+  "static/chunks/turbopack-27cd2632e7ee44c4.js"
+])
--- docs/static/chunks/src_app_hives_page_tsx_8fe6c6e8._.js
+++
@@ -1,8 +0,0 @@
-(globalThis.TURBOPACK_CHUNK_LISTS || (globalThis.TURBOPACK_CHUNK_LISTS = [])).push({
-    script: typeof document === "object" ? document.currentScript : undefined,
-    chunks: [
-  "static/chunks/node_modules_45e65580._.js",
-  "static/chunks/src_6ad13036._.js"
-],
-    source: "dynamic"
-});
