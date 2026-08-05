869f4e7a64d385e7d9605b4d456f099abf547a57
May <baermay98@gmail.com>
2026-08-05 10:43:15 +0200

    Revert base

--- src/lib/contexts.ts
+++ src/lib/contexts.ts
@@ -84,7 +84,7 @@ type AccentColourContextType = {
 };
 
 export const api = new Api({
-  baseUrl: "http://localhost:5138",
+  baseUrl: "http://home.mayiscoding.com/hivemime",
   securityWorker: (token) =>
     token ? { headers: { Authorization: `Bearer ${token}` } } : undefined,
   customFetch: async (input, init) => {
